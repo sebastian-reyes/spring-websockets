1b719502e52a239e7a290eff97fa6bb1845649c8
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-07-13 17:45:32 -0500

    Creando proyecto para chat en Spring con WebSockets

---
+++ src/main/java/com/sreyes/app/SpringBackendChatApplication.java
@@ -0,0 +1,13 @@
+package com.sreyes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendChatApplication.class, args);
+	}
+
+}
